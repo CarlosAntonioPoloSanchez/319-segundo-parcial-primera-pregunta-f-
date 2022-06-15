6557afb4ec02d473507c64b491580733936c959e
CarlosAntonioPoloSanchez <105763178+CarlosAntonioPoloSanchez@users.noreply.github.com>
2022-06-15 18:09:29 -0400

    Add files via upload

---
+++ Primera pregunta F.fs
@@ -0,0 +1,17 @@
+﻿// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+open System
+let rec fibonacci b =
+    if b<=1 then b else (fibonacci(b-1)+fibonacci(b-2)) 
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine("ingrese un numero")
+    let a = Convert.ToInt32(Console.ReadLine())
+    let b = ""
+    for i=1 to a do
+        let c= fibonacci(i)
+        Console.WriteLine(c)
+    let y = Console.ReadLine()
+    printfn "%A" argv
+    0 // devolver un código de salida entero
+    
